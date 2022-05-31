612fc0e9b2ce16a6459814786cd4709a4411d948
coffeemori <coffeemori92@naver.com>
2022-05-31 21:42:32 +0900

    로그인 페이지

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,12 +1,126 @@
-import React from 'react';
-import {Text, View} from 'react-native';
+import {NativeStackScreenProps} from '@react-navigation/native-stack';
+import React, {useCallback, useRef, useState} from 'react';
+import {
+  Alert,
+  Pressable,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from 'react-native';
+import {RootStackParamList} from '../../App';
 
-function SignIn() {
+type SignInScreenProps = NativeStackScreenProps<RootStackParamList, 'SignIn'>;
+
+function SignIn({navigation}: SignInScreenProps) {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const emailRef = useRef<TextInput | null>(null);
+  const passwordRef = useRef<TextInput | null>(null);
+
+  const onChangeEmail = useCallback((text: string) => {
+    setEmail(text);
+  }, []);
+
+  const onChangePassword = useCallback((text: string) => {
+    setPassword(text);
+  }, []);
+
+  const onSubmit = useCallback(() => {
+    if (!email || !email.trim()) {
+      return Alert.alert('알림', '이메일을 입력해주세요');
+    }
+    if (!password || !password.trim()) {
+      return Alert.alert('알림', '비밀번호를 입력해주세요');
+    }
+  }, [email, password]);
+
+  const toSignUp = useCallback(() => {
+    navigation.navigate('SignUp');
+  }, [navigation]);
+
+  const canGoNext = email && password;
+
   return (
     <View>
-      <Text>SignIn</Text>
+      <View style={styles.inputWrapper}>
+        <Text style={styles.label}>이메일</Text>
+        <TextInput
+          style={styles.textInput}
+          placeholder="이메일을 입력해주세요"
+          value={email}
+          onChangeText={onChangeEmail}
+          returnKeyType="next"
+          textContentType="emailAddress"
+          keyboardType="email-address"
+          autoComplete="email"
+          onSubmitEditing={() => {
+            passwordRef.current?.focus();
+          }}
+          blurOnSubmit={false}
+          ref={emailRef}
+        />
+      </View>
+      <View style={styles.inputWrapper}>
+        <Text style={styles.label}>비밀번호</Text>
+        <TextInput
+          style={styles.textInput}
+          placeholder="비밀번호를 입력해주세요"
+          value={password}
+          onChangeText={onChangePassword}
+          secureTextEntry
+          ref={passwordRef}
+          onSubmitEditing={onSubmit}
+        />
+      </View>
+      <View style={styles.buttonZone}>
+        <Pressable
+          style={
+            !canGoNext
+              ? styles.loginButton
+              : [styles.loginButton, styles.loginButtonActive]
+          }
+          onPress={onSubmit}
+          disabled={!canGoNext}>
+          <Text style={styles.loginButtonText}>로그인</Text>
+        </Pressable>
+        <Pressable onPress={toSignUp}>
+          <Text>회원가입</Text>
+        </Pressable>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  inputWrapper: {
+    padding: 20,
+  },
+  label: {
+    fontWeight: 'bold',
+    fontSize: 16,
+    marginBottom: 20,
+  },
+  textInput: {
+    padding: 5,
+    borderBottomWidth: StyleSheet.hairlineWidth,
+  },
+  buttonZone: {
+    alignItems: 'center',
+  },
+  loginButton: {
+    backgroundColor: 'gray',
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    borderRadius: 5,
+    marginBottom: 10,
+  },
+  loginButtonActive: {
+    backgroundColor: 'blue',
+  },
+  loginButtonText: {
+    color: 'white',
+  },
+});
 
 export default SignIn;
